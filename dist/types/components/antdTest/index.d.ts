80bb47f659b3925bed4bea1df1bdd287f51b77c1
guzhongren <guzhongren@live.cn>
2018-11-23 16:57:26 +0800

    pref(dist): 优化git dist目录

--- dist/types/components/antdTest/index.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-declare class Hello extends React.Component {
-    constructor(props: any);
-    render(): JSX.Element;
-}
-export default Hello;
--- dist/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import Hello from './components/antdTest/index';
-import 'antd/dist/antd.less';
-export { Hello };
-declare const _default: {
-    Hello: typeof Hello;
-};
-export default _default;
